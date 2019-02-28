e6d7e4be4817dd13a61b4660433e4af6a974a26c
henrycode23 <47737289+henrycode23@users.noreply.github.com>
2019-02-28 18:12:42 +0800

    creating controllers done

---
+++ classes/Bootstrap.php
@@ -0,0 +1,45 @@
+<?php
+class Bootstrap{
+  private $controller;
+  private $action;
+  private $request;
+
+  public function __construct($request){ // Set $request param 
+    $this->request = $request; // Assign the this request to the $request that comes in
+    if($this->request['controller'] == ""){ // Check if this request controller is empty
+      $this->controller = 'home'; // controller = home
+    } else{
+      $this->controller = $this->request['controller']; // controller = whatever comes in
+    }
+    if($this->request['action'] == ""){ // Check if this request action is empty
+      $this->action = 'index'; // action = index
+    } else{
+      $this->action = $this->request['action']; // action = whatever that comes in
+    }
+  }
+  
+  public function createController(){
+    // Check Class
+    if(class_exists($this->controller)){
+      $parents = class_parents($this->controller);
+      // Check Extend
+      if(in_array('Controller', $parents)){
+        if(method_exists($this->controller, $this->action)){
+          return new $this->controller($this->action, $this->request);
+        } else{
+          // Method Does Not Exist
+          echo '<h1>Method does not exist</h1>';
+          return;
+        }
+      } else{
+        // Base Controller Not Found
+        echo '<h1>Base controller not found</h1>';
+        return;
+      }
+    } else{
+      // Controller Class Does Not Exist
+      echo '<h1>Controller class does not exist</h1>';
+      return;
+    }
+  }
+}
